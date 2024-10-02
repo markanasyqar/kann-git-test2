85e5d6d7a3e37affe9ee9d529ce36e70619ff5f7
Muhammad Arkan <markanasyqar0704@gmail.com>
2024-10-02 11:13:03 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, vel. Incidunt rerum mollitia hic. Autem, temporibus porro? Provident quidem ab ullam optio, doloribus itaque tenetur dolores aliquid maxime perspiciatis harum?</p>
 </body>
 </html>
